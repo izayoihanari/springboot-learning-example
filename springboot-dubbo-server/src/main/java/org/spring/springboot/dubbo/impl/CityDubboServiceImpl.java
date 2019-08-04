63c6c4862ab61b5442589b8b743a74ea4f462362
ash <y14285790@163.com>
2019-08-04 17:28:10 +0800

    1212

--- springboot-dubbo-server/src/main/java/org/spring/springboot/dubbo/impl/CityDubboServiceImpl.java
+++ springboot-dubbo-server/src/main/java/org/spring/springboot/dubbo/impl/CityDubboServiceImpl.java
@@ -15,6 +15,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class CityDubboServiceImpl implements CityDubboService {
 
     public City findCityByName(String cityName) {
+
+
         return new City(1L,2L,"温岭","是我的故乡");
     }
 }
